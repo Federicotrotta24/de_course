d79a1520c04ef04d967bbd5c754d88b31c004463
Federico Trotta <federico.rtrotta@gmail.com>
2026-04-15 20:45:04 +0200

    Agregue las ultimas funciones para la extracion de los datos de yt

--- video_stats.py
+++ video_stats.py
@@ -63,14 +63,61 @@ def get_videos_id (playlist_id):
     except requests.exceptions.RequestException  as e:
         raise e
 
+        
+def extract_video_data(video_ids):
+    extracted_data = []
 
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id : video_id + batch_size]
+
+    try:
+        for batch in batch_list(video_ids, max_results):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails,snippet,statistics&id={video_ids_str}&key={API_KEY}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+def save_data_to_json(data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)    
 
 if __name__ == "__main__":    
     playlist_id = get_playlist_id(CHANNEL_HANDLE, API_KEY)
-    print(get_videos_id(playlist_id))
+    video_ids = get_videos_id(playlist_id)
+    video_data = extract_video_data(video_ids)
+    save_data_to_json(video_data, "video_data.json")
 
 
 
 
-
-base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}"
+base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxesults={max_results}&playlistId={playlist_id}&key={API_KEY}"
