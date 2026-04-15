7d34bc5f20dd6af2942705c548364c87b714ad1e
Federico Trotta <federico.rtrotta@gmail.com>
2026-04-15 18:24:54 +0200

    Add function to extract video IDs from YouTube playlist

--- video_stats.py
+++ video_stats.py
@@ -9,6 +9,9 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+max_results = 50
+
+
 url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
 
@@ -25,12 +28,49 @@ def get_playlist_id(channel_handle, api_key):
 
 
 
-    
+def get_videos_id (playlist_id):
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}"
+
+    try:
+
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+    except requests.exceptions.RequestException  as e:
+        raise e
 
 
 
 if __name__ == "__main__":    
     playlist_id = get_playlist_id(CHANNEL_HANDLE, API_KEY)
-    print(playlist_id)
+    print(get_videos_id(playlist_id))
 
 
+
+
+
+base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}"
